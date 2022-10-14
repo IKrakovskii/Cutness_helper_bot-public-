b877c3632d036c330ddc034a6e0816362174eb64
ivkrak <55966557+IKrakovskii@users.noreply.github.com>
2022-10-14 10:13:35 +0800

    main

---
+++ functions.py
@@ -0,0 +1,11 @@
+Token = '5709212625:AAFoPLNW8HJaMuEX8PN4ybPhsI6pVR0ow_M'
+
+cat = '''
+          ／＞　 フ          
+　　　　　| 　_　 _|
+　 　　　／`ミ _x 彡
+　　 　 /　　　 　 |
+　　　 /　 ヽ　　 ﾉ
+　／￣|　　 |　|　|
+　| (￣ヽ＿_ヽ_)_)
+'''
---
+++ main.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup
+from functions import Token, cat
+
+bot = Bot(token=Token)
+dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print('Бот\n@first_edu_aiogram_bot\nзапущен', cat)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
