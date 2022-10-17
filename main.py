9a03199928b05cf85411840b7f4128ad1fa352b3
ivkrak <iv.krak@gmail.com>
2022-10-17 14:45:52 +0800

    Снова начал использовать телебота(безрезультатно)

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-import telebot
-from functions import Token, cat, out
-
-bot = telebot.TeleBot(Token)
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def on_startup(_):
-    print('Бот\n@cuteness_helper_bot\nзапущен', cat)
+x = np.array([i for i in range(1000)][::-1])
+y = np.array([i for i in range(1000)])
+z = np.array([i for i in range(1000)])
 
-
-@bot.message_handler(commands=['start'])
-def welcome(message):
-    bot.answer_callback_query(message.chat.id,
-                              'Привет, этот бот создан, чтобы пересылать сообщения в отдельный канал')
-
-
-@bot.message_handler(commands=['F'])
-def forward(message):
-    bot.forward_message(message_id=message.id, from_chat_id='-1001523047524')
-
-
-if __name__ == '__main__':
-    bot.infinity_polling()
+plt.plot(x, y, z, '_')
+plt.show()
